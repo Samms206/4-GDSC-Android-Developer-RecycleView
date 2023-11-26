dd791b0ce12e5026371676e49f6ea53378a8d7d9
Samms206 <sh2069936@gmail.com>
2023-11-26 23:11:41 +0700

    updated DetailMovieActivity

--- app/src/main/java/com/example/testing/DetailMovieActivity.kt
+++ app/src/main/java/com/example/testing/DetailMovieActivity.kt
@@ -9,7 +9,17 @@ class DetailMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_movie)
+        val name = intent.getStringExtra("movieName")
+        val description = intent.getStringExtra("movieDesc")
+        val image =  intent.getIntExtra("movieImage", 0)
 
+        val title = findViewById<TextView>(R.id.tv_title)
+        val desc = findViewById<TextView>(R.id.tv_desc)
+        val img_banner = findViewById<ImageView>(R.id.img_views)
+
+        title.text = name
+        desc.text = description
+        img_banner.setImageResource(image)
 
     }
 }
