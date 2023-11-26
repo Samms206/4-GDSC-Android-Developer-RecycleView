ad2dbff7d1c6d49574873ecfea29ed3324860e1b
Samms206 <sh2069936@gmail.com>
2023-11-26 23:08:50 +0700

    updated movieData

--- app/src/main/java/com/example/testing/movieData.kt
+++ app/src/main/java/com/example/testing/movieData.kt
@@ -1,5 +1,21 @@
 package com.example.testing
 
 object movieData {
-
+    val listMovie = listOf<movies>(
+        movies(
+            "La La Land",
+            "La La Land adalah film drama komedi musikal romansa Amerika Serikat tahun 2016 yang ditulis dan disutradarai Damien Chazelle dan dibintangi Ryan Gosling, Emma Stone, John Legend, dan Rosemarie DeWitt. Film ini berkisah tentang seorang musisi dan calon aktris yang bertemu dan jatuh cinta di Los Angeles. Judulnya mengacu pada singkatan nama kota Los Angeles dan ungkapan yang berarti dunia impian. La La Land tayang perdana di Festival Film Venesia pada tanggal 31 Agustus 2016 dan dirilis di Amerika Serikat tanggal 9 Desember 2016 oleh Summit Entertainment. Film ini mendapat banyak pujian dengan pendapatan \$90 juta di seluruh dunia. American Film Institute memasukkan film ini ke daftar sepuluh film terbaik tahun 2016.[4] National Board of Review juga memasukkan film ini ke daftar sepuluh film studio terbaik tahun 2016.[5] Film ini juga memenangi Critics' Choice Movie Award for Best Picture[6] dan tujuh Golden Globe: Film Terbaik – Musikal atau Komedi, Sutradara Terbaik, Aktor Terbaik untuk Gosling, Aktris Terbaik untuk Stone, Skenario Terbaik, Musik Orisinal Terbaik, dan Lagu Orisinal Terbaik (City of Stars)",
+            R.drawable.lalaland
+        ),
+        movies(
+            "Barbie",
+            "Film ini diawali dengan Narator (Helen Mirren) yang menjelaskan dampak sosial dari boneka Barbie terhadap sejarah, disertai dengan klip Barbie asli tahun 1959 yang menjulang tinggi di atas lanskap padang pasir, sementara gadis-gadis kecil yang bermain dengan boneka mulai menghancurkannya.",
+            R.drawable.barbie
+        ),
+        movies(
+            "Oppenheimer",
+            "Oppenheimer (/ˈɒpənˌhaɪmər/) adalah film biografi drama sejarah tahun 2023 yang disutradarai oleh Christopher Nolan dan diproduseri oleh Christopher Nolan, Emma Thomas dan Charles Roven. Naskah film ini ditulis oleh Christopher Nolan berdasarkan buku American Prometheus tahun 2005 karya Kai Bird dan Martin J. Sherwin. Film ini dibintangi oleh Cillian Murphy, Emily Blunt, Matt Damon, Robert Downey Jr., Florence Pugh, Josh Hartnett, Casey Affleck, Rami Malek dan Kenneth Branagh. Proyek film Oppenheimer diumumkan pada bulan September 2021 setelah Universal Pictures memenangkan penawaran untuk skenario film Christopher Nolan. Cillian Murphy dipilih sebagai pemeran J. Robert Oppenheimer pada bulan Oktober, sedangkan pemeran utama lainnya bergabung dengan proyek film pada bulan November 2021 hingga bulan April 2022. Proses praproduksi film ini berlangsung pada bulan Januari 2022 dan pembuatan film berlangsung mulai bulan Februari hingga bulan Mei. Film ini dibuat dengan menggabungkan IMAX 65 mm dan format besar 65 mm serta fotografi film hitam putih IMAX. Seperti karya-karya sebelumnya, Christopher Nolan menggunakan efek praktis yang luas dan pencitraan hasil komputer yang minim. Film Oppenheimer merupakan film kolaborasi kedua Cillian Murphy dan Emily Blunt setelah film A Quiet Place: Part II. Film ini merupakan film kolaborasi kedua Emily Blunt dan Matt Damon setelah film The Adjustment Bureau. Film ini ditayangkan secara perdana di Le Grand Rex di Paris pada tanggal 11 Juli 2023 dan dirilis di Amerika Serikat dan Britania Raya pada tanggal 21 Juli 2023. Jadwal perilisannya yang secara bersamaan dengan film Barbie karya Greta Gerwig memunculkan fenomena \"Barbenheimer\" di media sosial sehingga mendorong penonton untuk menonton kedua film tersebut. Film ini mendapatkan review positif dari para kritikus.\n",
+            R.drawable.heimer
+        )
+    )
 }
