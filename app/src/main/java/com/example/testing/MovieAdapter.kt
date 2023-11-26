7a5a6f7ad4569d59b027d0a6f470b275dfd7f484
Samms206 <sh2069936@gmail.com>
2023-11-26 23:09:57 +0700

    updated MovieAdapter

--- app/src/main/java/com/example/testing/MovieAdapter.kt
+++ app/src/main/java/com/example/testing/MovieAdapter.kt
@@ -9,6 +9,41 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class MovieAdapter(private val listMovie: List<movies>){
+class MovieAdapter(private val listMovie: List<movies>):RecyclerView.Adapter<MovieAdapter.ListViewHolder>(){
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: movies)
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listMovie[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listMovie[holder.adapterPosition])
+        }
+    }
+
+    override fun getItemCount(): Int = listMovie.size
+
+    class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_view)
+        val tvName: TextView = itemView.findViewById(R.id.tv_title)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_desc)
+        init {
+            tvDescription.maxLines = 2
+            tvDescription.ellipsize = TextUtils.TruncateAt.END
+        }
+    }
 
 }
